510213393d9cae58c5a8cccd21f322f7bba4e0cf
savan <vsavan7@gmail.com>
2021-02-19 13:22:36 -0600

    add coord repo

---
+++ examples/trials/cifar10_grad_match/cords/selectionstrategies/supervisedlearning/__init__.py
@@ -0,0 +1,8 @@
+from .craigstrategy import CRAIGStrategy
+from .dataselectionstrategy import DataSelectionStrategy
+from .glisterstrategy import GLISTERStrategy
+from .randomstrategy import RandomStrategy
+from .submodularselectionstrategy import SubmodularSelectionStrategy
+from .ompgradmatchstrategy import OMPGradMatchStrategy
+
+__version__ = '0.0.4'
